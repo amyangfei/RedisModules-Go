f0dd30f0fda0df80885f1b9d6001ba2d4fd20a52
amyangfei <amyangfei@gmail.com>
2016-07-17 15:29:40 +0800

    Add interaction between C pointer and go slice

--- example/basic/hello_module.c
+++ example/basic/hello_module.c
@@ -7,10 +7,34 @@ int EchoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   if (argc < 2) return RedisModule_WrongArity(ctx);
   RedisModule_AutoMemory(ctx);
   size_t len;
-  const char *str = RedisModule_StringPtrLen(argv[1], &len);
-  char *dst = strndup(str, len);
+  char *dst = RedisModule_Strdup(RedisModule_StringPtrLen(argv[1], &len));
   GoString *r = GoEcho(dst);
   RedisModuleString *rm_str = RedisModule_CreateString(ctx, (*r).p, (*r).n);
+  return RedisModule_ReplyWithString(ctx, rm_str);
+}
+
+/* ECHO2 <string> - Echo back a string sent from the client */
+int Echo2Command(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  if (argc < 2) return RedisModule_WrongArity(ctx);
+  RedisModule_AutoMemory(ctx);
+  size_t len;
+  char *dst = RedisModule_Strdup(RedisModule_StringPtrLen(argv[1], &len));
+  GoString *r = GoEcho2(dst, len);
+  RedisModuleString *rm_str = RedisModule_CreateString(ctx, (*r).p, (*r).n);
+  return RedisModule_ReplyWithString(ctx, rm_str);
+}
+
+/* ECHO3 <string> - Echo back a string sent from the client */
+int Echo3Command(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  if (argc < 2) return RedisModule_WrongArity(ctx);
+  RedisModule_AutoMemory(ctx);
+  size_t len;
+  char *dst = RedisModule_Strdup(RedisModule_StringPtrLen(argv[1], &len));
+  char *r = GoEcho3(dst, len);
+  size_t n = strlen(r);
+  RedisModuleString *rm_str = RedisModule_CreateString(ctx, r, n);
+  // free memory allocated in Golang stack
+  free(r);
   return RedisModule_ReplyWithString(ctx, rm_str);
 }
 
@@ -20,6 +44,12 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
     return REDISMODULE_ERR;
   }
   if (RedisModule_CreateCommand(ctx, "hello.echo", EchoCommand, "readonly", 1,1,1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+  if (RedisModule_CreateCommand(ctx, "hello.echo2", Echo2Command, "readonly", 1,1,1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+  if (RedisModule_CreateCommand(ctx, "hello.echo3", Echo3Command, "readonly", 1,1,1) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 }
--- example/basic/hello_module.go
+++ example/basic/hello_module.go
@@ -1,12 +1,37 @@
 package main
 
+// #include <stdlib.h>
 // #include "redismodule.h"
 import "C"
+import "unsafe"
 
 //export GoEcho
 func GoEcho(s *C.char) *string {
 	gostr := (C.GoString(s) + " from golang")
 	return &gostr
+}
+
+//export GoEcho2
+func GoEcho2(s *C.char, length C.int) *string {
+	slice := C.GoBytes(unsafe.Pointer(s), length)
+	slice = append(slice, []byte(" from golang2")...)
+	gostr := string(slice)
+	return &gostr
+}
+
+//export GoEcho3
+func GoEcho3(s *C.char, length C.int) unsafe.Pointer {
+	slice := C.GoBytes(unsafe.Pointer(s), length)
+	slice = append(slice, []byte(" from golang3")...)
+
+	p := C.malloc(C.size_t(len(slice)))
+	// free memory in c code
+	// defer C.free(p)
+
+	cBuf := (*[1 << 30]byte)(p)
+	copy(cBuf[:], slice)
+
+	return p
 }
 
 func main() {}
