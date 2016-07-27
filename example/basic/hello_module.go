de3067423817f46a7e40581ef5bd1dc759c51abf
amyangfei <amyangfei@gmail.com>
2016-07-27 23:37:02 +0800

    Add some comment to example. Slice append witout []byte convert

--- example/basic/hello_module.go
+++ example/basic/hello_module.go
@@ -36,23 +36,31 @@ func ZeroCopySlice(p unsafe.Pointer, n int) *Slice {
 }
 
 //export GoEcho
+// c->go: Convert C string to Go string
+// go->c: return Go string pointer
 func GoEcho(s *C.char) *string {
-	gostr := (C.GoString(s) + " from golang")
+	gostr := (C.GoString(s) + " from golang1")
 	return &gostr
 }
 
 //export GoEcho2
+// c->go: Convert C array with explicit length to Go []byte using C.GoBytes
+// go->c: return Go string pointer
 func GoEcho2(s *C.char, length C.int) *string {
 	slice := C.GoBytes(unsafe.Pointer(s), length)
-	slice = append(slice, []byte(" from golang2")...)
+	slice = append(slice, " from golang2"...)
 	gostr := string(slice)
 	return &gostr
 }
 
 //export GoEcho3
+// c->go: Convert C array with explicit length to Go []byte using C.GoBytes
+// go->c: malloc the C buffer, and make a single copy into that buffer. It is
+// important to keep in mind that the Go garbage collector will not interact
+// with this data, and that if it is freed from the C side of things
 func GoEcho3(s *C.char, length C.int) unsafe.Pointer {
 	slice := C.GoBytes(unsafe.Pointer(s), length)
-	slice = append(slice, []byte(" from golang3")...)
+	slice = append(slice, " from golang3"...)
 
 	p := C.malloc(C.size_t(len(slice)))
 	// free memory in c code
@@ -65,13 +73,16 @@ func GoEcho3(s *C.char, length C.int) unsafe.Pointer {
 }
 
 //export GoEcho4
+// c->go: Convert C array with explicit length to Go []byte using C.GoBytes
+// go->c: return unsafe.Pointer and length
 func GoEcho4(s *C.char, length C.int) (unsafe.Pointer, int) {
 	slice := C.GoBytes(unsafe.Pointer(s), length)
-	slice = append(slice, []byte(" from golang4")...)
+	slice = append(slice, " from golang4"...)
 	return unsafe.Pointer(&(slice[0])), len(slice)
 }
 
 //export GoEcho5
+// error handler example
 func GoEcho5(s *C.char) (*string, *string) {
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
@@ -87,10 +98,11 @@ func GoEcho5(s *C.char) (*string, *string) {
 }
 
 //export GoEcho6
+// c->go: using reflect to bind C memory to Go resource without memory copy.
+// go->c: return unsafe.Pointer and length
 func GoEcho6(s *C.char, length C.int) (unsafe.Pointer, int) {
 	zslice := ZeroCopySlice(unsafe.Pointer(s), int(length))
-	// slice := C.GoBytes(unsafe.Pointer(s), length)
-	slice := append(zslice.Data, []byte(" from golang6")...)
+	slice := append(zslice.Data, " from golang6"...)
 	return unsafe.Pointer(&(slice[0])), len(slice)
 }
 
